1c2e3de1d3facdf4cb8f8b5581c19e717d91e22b
Adam Wall <agcwall@gmail.com>
2019-06-05 22:35:35 -0600

    print every event before drawing

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,16 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.Text(unpack)
+
 import Graphics.Blank
+
+main = blankCanvas options program
 
 options :: Options
 options = Options {
     port = 3000,
+    -- this is all the possible events
     events = [
           "keypress"
         , "keydown"
@@ -22,10 +27,17 @@ options = Options {
     weak = False
 }
 
-main = blankCanvas options $ \ context -> do
-  send context $ do
-    moveTo(50,50)
-    lineTo(200,100)
-    lineWidth 10
-    strokeStyle "red"
-    stroke ()
+program :: DeviceContext -> IO ()
+program context = do
+  event <- wait context
+  putStrLn (unpack (eType event))
+  send context draw
+  program context
+
+draw :: Canvas ()
+draw = do
+  moveTo(50,50)
+  lineTo(200,100)
+  lineWidth 10
+  strokeStyle "red"
+  stroke ()
